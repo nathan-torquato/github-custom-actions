8ef45bbf40ec06972ebcb99f15e53117d8654cfb
Nathan Torquato <n.torquato.silva@gmail.com>
2023-04-28 10:04:42 -0300

    feat: deploy to S3 bucket

--- .github/actions/deploy-to-s3/main.js
+++ .github/actions/deploy-to-s3/main.js
@@ -1,9 +1,19 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+// const github = require('@actions/github')
+const { exec } = require('@actions/exec')
 
 function run() {
-  core.notice('Deploying to S3 from custom JS action')
+  core.notice('🚀 Deploying to S3 from custom JS action')
+
+  const bucket = core.getInput('bucket', { required: true })
+  const region = core.getInput('region', { required: true })
+  const distFolder = core.getInput('dist-folder', { required: true })
+
+  console.log(bucket, region, distFolder)
+
+  exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${region}`)
+
+  core.notice('✅ Successfully Deployed to S3 from custom JS action')
 }
 
 run()
